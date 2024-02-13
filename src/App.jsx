db9b1a6a8f44d468a1ddb3df4d2ca741c68d3a8b
sudeera-madushan <sudeeramadushan888@gmail.com>
2024-02-13 18:22:12 +0530

    design posts ui

--- src/App.jsx
+++ src/App.jsx
@@ -16,16 +16,26 @@ function App() {
         // console.log(comments);
     }
     return (
-        <div>
-            {/*<button onClick={show}>show</button>*/}
-        <div className={"grid grid-cols-4 gap-6 p-10"}>
+        <div className={"bg-[rgba(31,41,55,1)]"}>
+            <button onClick={show}>show</button>
+        <div className={"grid grid-cols-5 gap-4 p-9"}>
             {posts.map((post) => (
-                <div key={post.id} className={"bg-gray-100 rounded shadow-xl"}>
-                    <div className={"border-b-2 p-2"}>
-                        <h2 className="text-1xl font-bold text-gray-600 flex ">{post.title}</h2>
+                <div key={post.id} className={"cursor-pointer bg-[rgba(239,246,255,0.1)] rounded-md hover:bg-[rgba(239,246,255,0.2)] p-2 duration-300"}>
+                    <div className={" p-2"}>
+                        <h2 className=" text-[18px] font-sans font-bold text-[rgba(217,119,6,1)] flex domine-font">{
+                            post.title.length > 18 ? post.title.charAt(0).toUpperCase() + post.title.substring(0, 18).slice(1) + "..."
+                                : post.title.charAt(0).toUpperCase() + post.title.slice(1)
+                        }</h2>
                     </div>
-                    <div className={"p-2"}>
-                        <p className={"text-xl text-gray-500"}>{post.body}</p>
+                    <div className={"px-2"}>
+                        <p className={"text-[15px] text-white maven-pro-font h-[90px]"}>{
+                            post.body.length > 90 ? post.body.charAt(0).toUpperCase() + post.body.substring(0, 90).slice(1) + "..."
+                                : post.body.charAt(0).toUpperCase() + post.body.slice(1)
+                        }</p>
+                    </div>
+                    <div className={"w-11/12 rounded border-gray-300 border m-2 mt-3"}/>
+                    <div>
+                        <p className={"p-2 pt-0 text-gray-200 font-sans text-sm"}>Author: sdjansjdna jksd </p>
                     </div>
                 </div>
             ))}
